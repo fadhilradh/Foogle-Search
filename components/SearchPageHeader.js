c0eb5b2ca36ae048c9e3d12c0594730a9ac2abde
Fadhil Radhian <fadhilradhian@gmail.com>
2021-06-16 18:39:18 +0700

    fix styling

--- components/SearchPageHeader.js
+++ components/SearchPageHeader.js
@@ -17,20 +17,29 @@ function SearchPageHeader() {
   };
   return (
     <header className="sticky top-0 bg-white">
-      <div className="flex w-full p-6 items-center">
-        <Image
-          className="cursor-pointer"
-          src="/logo.png"
-          width={90}
-          height={30}
-          onClick={() => router.push("/")}
-        />
+      <div className="flex flex-col w-full p-6 items-center sm:items-start">
+        <div className="flex w-full justify-center sm:justify-start mb-3">
+          <div className="flex-1 sm:hidden"></div>
+          <Image
+            className="cursor-pointer flex-1 "
+            src="/logo.png"
+            width={120}
+            height={30}
+            onClick={() => router.push("/")}
+          />
+          <div className="flex-1 flex justify-end ">
+            <Avatar className="" url={"/fadhil.jpeg"} />
+          </div>
+        </div>
 
-        <form className="flex flex-grow max-w-3xl items-center border border-gray-300 rounded-full px-6 py-3 ml-10 mr-5 shadow-google">
+        <form
+          className="flex sm:ml-36 md:ml-10 sm:justify-start w-full flex-grow max-w-3xl items-center border border-gray-300 
+        rounded-full px-6 py-3 sm:mr-5 shadow-google"
+        >
           <input
             type="text"
             ref={searchInputRef}
-            className="flex-grow w-full focus:outline-none"
+            className="flex-grow focus:outline-none"
             defaultValue={router.query.term}
           />
           <XIcon
@@ -41,7 +50,6 @@ function SearchPageHeader() {
           <SearchIcon className="h-7 text-blue-500 ml-3 hidden sm:inline-flex" />
           <button className="hidden" type="submit" onClick={search} />
         </form>
-        <Avatar className="ml-auto" url={"/fadhil.jpeg"} />
       </div>
       <HeaderTabs />
     </header>
